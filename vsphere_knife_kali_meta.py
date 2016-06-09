443f71debd2a4e541598b56e1fe6f005b4b33917
Juan Ortega <juano@cpp.edu>
2016-06-09 02:33:40 -0700

    update 2

--- vsphere_knife_kali_meta.py
+++ vsphere_knife_kali_meta.py
@@ -1,16 +1,23 @@
 import subprocess
 from random import choice
 from string import ascii_uppercase
+from subprocess import Popen, DEVNULL
 
 
 def main():
-   # for _ in range(instances()):
-   #      vmnames = generate_vm_name()
-   #      for name in vmnames:
-   #          deploy_kali(name)
-   #          deploy_meta(name)
-   print(generate_vm_name())
-    #print(instances())
+   for _ in range(instances()):
+        vmnames = [generate_vm_name()]
+        for name in vmnames:
+            print('Preparing to deploy {}-Kali'.format(name))
+            deploy_kali(name)
+            print('{}-Kali deployed successfully'.format(name))
+            power_on('{}-Kali'.format(name))
+            print('Preparing to deploy {}-Metasploitable'.format(name))
+            deploy_meta(name)
+            print('{}-Metasploitable deployed successfully'.format(name))
+            power_on('{}-Metasploitable'.format(name))
+
+print('Operation completed successfully.')
 
 
 def instances():
@@ -18,8 +25,12 @@ def instances():
     number_of_instances = int(number_of_instances)
     return number_of_instances
 
-def power_on():
-    pass
+
+def power_on(self):
+    try:
+        subprocess.call('knife vsphere vm state {} --state on'.format(self), shell=True)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
 
 
 def reset_ip():
@@ -32,19 +43,15 @@ def generate_vm_name():
 
 def deploy_kali(self):
     try:
-        subprocess.check_call('knife vsphere vm clone "Kali{}" --template SP_Kali'.format(self), shell=True)
-    except:
-        pass
-    # except subprocess.CalledProcessError as e:
-    #     raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+        subprocess.call('knife vsphere vm clone "{}-Kali" --template SP-Kali'.format(self), shell=True, stdout=DEVNULL, stderr=DEVNULL)
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
 
 
 def deploy_meta(self):
     try:
-        subprocess.check_call('knife vsphere vm clone "Meta{}" --template Metasploitable'.format(self), shell=True)
-    except:
-        pass
-    # except subprocess.CalledProcessError as e:
-    #     raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
+        subprocess.call('knife vsphere vm clone "{}-Metasploitable" --template GM-Metasploitable'.format(self), shell=True, stdout=DEVNULL, stderr=DEVNULL )
+    except subprocess.CalledProcessError as e:
+        raise RuntimeError("command '{}' return with error (code {}): {}".format(e.cmd, e.returncode, e.output))
 
 if __name__ == "__main__": main()
